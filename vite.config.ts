feae27a3c6cdd58d4ccde1329be70ee0c8c6ad1f
Gabriel de Souza Rossetto <148262625+R0SSETT0@users.noreply.github.com>
2026-03-19 16:28:16 -0300

    Change server port to 54322 in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
-    port: 8080,
+    port: 54322,
     allowedHosts: ['all'],
     hmr: {
       overlay: false,
