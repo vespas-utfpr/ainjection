8c6639e603c8aaf68d42c01e2c38ae9aad9f0c4d
augustograeml <augusto.graeml@gmail.com>
2026-03-19 15:20:41 -0300

    atualizei o vite.config.js para aceitar todos os hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "all",
     port: 8080,
     hmr: {
       overlay: false,
