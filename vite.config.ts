6e287268f4169c5c2e8bc8cd173d527539139e28
Gabriel de Souza Rossetto <148262625+R0SSETT0@users.noreply.github.com>
2026-03-19 16:11:47 -0300

    Change allowedHosts to array format

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
     port: 8080,
-    allowedHosts: "all",
+    allowedHosts: ['all'],
     hmr: {
       overlay: false,
     },
